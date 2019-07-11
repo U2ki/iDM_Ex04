ce045027a1f6ad4c7a97d3624ea0dbc253682163
s1260054@u-aizu.ac.jp <k7na27>
2019-07-11 18:26:15 +0900

    Update game.c

--- game.c
+++ game.c
@@ -4,7 +4,7 @@
 
 
 int main(void){
-int i,j,n,k;
+int i,j,n;
 char name[20];
 
 printf("What is your name?\n>");
@@ -16,10 +16,9 @@ srand((unsigned)time(NULL));
 
 i = rand()%6+1;
 j = rand()%6+1;
-k = rand()%6+1;
-printf("Die 1:%d\nDie 2:%d\nDie 3:%d\nTotal value:%d\n",i,j,k,i+j+k);
+printf("Die 1:%d\nDie 2:%d\nTotal value:%d\n",i,j,i+j);
 
-if((i+j+k)>=10) printf("%s won!\n",name );
+if((i+j)>=7) printf("%s won!\n",name );
 else printf("%s lose!\n",name );
 
 return 0;
